7ba4d6298ca086db03bc5e3cec127f60592e6f09
Nuttaphon Rodphakdeekul <eparis@atlassian.com>
2017-02-22 12:39:23 +0700

    add mainpage and style css

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from . import views # for index
 urlpatterns = [
+    url(r'^$', views.index,name='index'),
     url(r'^account/', include('account.urls')),
     url(r'^admin/', admin.site.urls),
 ]
